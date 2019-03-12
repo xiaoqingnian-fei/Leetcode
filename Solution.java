8528518551b3228f5a7e2b11faa8da550581c16e
xiaoqingnian-fei <48284475+xiaoqingnian-fei@users.noreply.github.com>
2019-03-12 17:05:47 +0800

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Solution {
     public int reverse(int x) {
     	int key=0;
@@ -13,3 +15,12 @@ class Solution {
     }
 }
 //Integer.MAX_VALUE/10  32位整数最大范围为Integer.MAX_VALUE，但是针对于该算法中的key，key=key*10，所有要/10即可.
+public class Main {
+
+	public static void main(String[] args) {
+        Solution x=new Solution();
+		int a=in.nextInt();
+		System.out.println(x.reverse(a));
+		System.out.println(Integer.MAX_VALUE/10);
+	}
+}
