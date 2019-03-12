e1acec9e0f7f07d36effd3610b23da1d9e07860c
xiaoqingnian-fei <48284475+xiaoqingnian-fei@users.noreply.github.com>
2019-03-12 17:02:28 +0800

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int reverse(int x) {
+    	int key=0;
+    	while(x!=0) {
+    		int y=0;
+    		y=x%10;
+    		x=x/10;
+    		if (key > Integer.MAX_VALUE/10 || (key == Integer.MAX_VALUE / 10 && y > 7)) return 0;
+            if (key < Integer.MIN_VALUE/10 || (key == Integer.MIN_VALUE / 10 && y < -8)) return 0;
+            key=key*10+y;
+    	}
+    	return key;
+    }
+}
+//Integer.MAX_VALUE/10  32位整数最大范围为Integer.MAX_VALUE，但是针对于该算法中的key，key=key*10，所有要/10即可.
